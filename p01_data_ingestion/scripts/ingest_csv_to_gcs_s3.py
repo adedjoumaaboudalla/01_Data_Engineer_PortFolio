c9b9d3c9e18034ce21bee250318f33b731c620f4
AAD <adedjoumaaboudalla@gmail.com>
2025-09-07 20:37:20 +0200

    Ajout de l'orchestration deu chargement et s=du stockage des données pokemon sur GCS

--- p01_data_ingestion/scripts/ingest_csv_to_gcs_s3.py
+++ p01_data_ingestion/scripts/ingest_csv_to_gcs_s3.py
@@ -11,7 +11,7 @@ sys.path.append(os.path.abspath(os.path.join("..", "..")))
 
 
 
-def save_df_to_gcs_bucket(df: pd.DataFrame, object_name = "csv/movies.csv") -> bool:
+def save_df_to_gcs_bucket(df: pd.DataFrame, object_name = "csv/movies.csv", credentials=None) -> bool:
     """
     Stockage du DataFrame sur GCS
 
@@ -26,12 +26,46 @@ def save_df_to_gcs_bucket(df: pd.DataFrame, object_name = "csv/movies.csv") -> b
         bucket_name = "de-01-data-ingestion"
 
         # Initialise client
-        client = storage.Client()  # nécessite GOOGLE_APPLICATION_CREDENTIALS
+        if credentials:
+            client = storage.Client(credentials=credentials)
+        else :
+            client = storage.Client()
         bucket = client.bucket(bucket_name)
         blob = bucket.blob(object_name + str(uuid.uuid4()))
 
         # Upload CSV
         blob.upload_from_string(df.to_csv(index=False), content_type="text/csv")
+        info("Upload réussi !")
+    except Exception as e:
+        error(f"File Creation failed with {e}")
+        return False
+
+    return True
+
+def save_text_to_gcs_bucket(text: str, object_name, credentials=None) -> bool:
+    """
+    Stockage d'un contenue text sur GCS
+
+    Args:
+        df(pd.DataFrame): DataFrame à stocker
+        object_name(str): Object Name
+    Returns
+        bool
+    """
+    try:
+
+        bucket_name = "de-01-data-ingestion"
+
+        # Initialise client
+        if credentials:
+            client = storage.Client(credentials=credentials)
+        else :
+            client = storage.Client()  # nécessite GOOGLE_APPLICATION_CREDENTIALS
+        bucket = client.bucket(bucket_name)
+        blob = bucket.blob("text/" + object_name)
+
+        # Upload CSV
+        blob.upload_from_string(text, content_type="text/plain")
         info("Upload réussi !")
     except Exception as e:
         error(f"File Creation failed with {e}")
